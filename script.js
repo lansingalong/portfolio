357dad6542f6952b70eb8f98ae9b02ef546c5825
Lansing Cai <lansingc418@gmail.com>
2016-10-30 02:21:45 -0400

    install isotope for masonry

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
-// init Masonry
-var $grid = $('.grid').masonry({
-	itemSelector: '.grid-item',
-	resize: true,
-	fitWidth: true,
-	percentPosition: true
+var $grid;
+function makeMosaicLayout(){
+	// init Masonry
+	$grid = $('.grid').isotope({
+	  itemSelector: '.grid-item',
+	  layoutMode: 'masonry',
+	  resize: true,
+	  fitWidth: true,
+	  percentPosition: true,
+	  filter: function () {
+	  	return ".category-" + getCategory();
+	  }
+	});
+
+
+	// layout Masonry after each image loads
+	$grid.imagesLoaded().progress( function() {
+		$grid.isotope('layout');
+	});
+}
+
+
+function getCategory() {
+	hash = window.location.hash.slice(1)
+	if(hash) {
+		if (["all", "featured", "motion", "ux", "web", "mobile", "illustration"].indexOf(hash) > -1) {
+			return hash;
+		} else {
+			return "all";
+		}
+	} else {
+		return "all";
+	}
+}
+
+$(function() {
+	makeMosaicLayout();
 });
-// layout Masonry after each image loads
-$grid.imagesLoaded().progress( function() {
-	$grid.masonry('layout');
-});
+
+window.onhashchange = function() {
+	$grid.isotope({
+		filter: ".category-" + getCategory()
+	});
+}
